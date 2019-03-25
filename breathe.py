4f4579aa657aaf3148729aaf1358eef2ba268249
Alex <alecsandru.g@gmail.com>
2019-03-25 02:21:08 +0200

    Fix ease func

--- breathe.py
+++ breathe.py
@@ -11,11 +11,11 @@ try:
     while 1:
         for dc in range(0, 101, 5):
             ease = pytweening.easeInQuad(dc / 100)
-            p.ChangeDutyCycle(ease)
+            p.ChangeDutyCycle(ease * 100)
             time.sleep(0.1)
         for dc in range(100, -1, -5):
             ease = pytweening.easeOutQuad(dc / 100)
-            p.ChangeDutyCycle(ease)
+            p.ChangeDutyCycle(ease * 100)
             time.sleep(0.1)
 except KeyboardInterrupt:
     pass
