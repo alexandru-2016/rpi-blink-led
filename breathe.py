901447febf4eaa272f839f5eebb84fe3918661b8
Alex <alecsandru.g@gmail.com>
2019-03-25 02:33:38 +0200

    adjust ease params

--- breathe.py
+++ breathe.py
@@ -9,15 +9,15 @@ p = GPIO.PWM(18, 50)  # channel=12 frequency=50Hz
 p.start(0)
 try:
     while 1:
-        for dc in range(0, 101, 5):
+        for dc in range(0, 101, 2):
             ease = pytweening.easeInQuad(dc / 100)
             p.ChangeDutyCycle(ease * 100)
-            time.sleep(0.2)
+            time.sleep(0.05)
         
         for dc in range(100, -1, -5):
             ease = pytweening.easeOutQuad(dc / 100)
             p.ChangeDutyCycle(ease * 100)
-            time.sleep(0.2)
+            time.sleep(0.1)
 
         # stay asleep a while longer when dark
         time.sleep(5)
