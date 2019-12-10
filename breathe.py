f083da2970f0d913666be5653ad93205ec5db57f
Alex <alecsandru.g@gmail.com>
2019-12-10 20:08:59 +0000

    Add astral lib

--- breathe.py
+++ breathe.py
@@ -3,8 +3,32 @@ import time
 import RPi.GPIO as GPIO
 
 import pytweening
+from astral import Astral
 
 LED_PIN=18
+
+def setup_sun():
+    city_name = 'Bucharest'
+    a = Astral()
+    a.solar_depression = 'civil'
+    city = a[city_name]
+    print('Solar data for %s/%s\n' % (city_name, city.region))
+
+    timezone = city.timezone
+    print('Timezone: %s' % timezone)
+
+
+def setup_gpio():
+    global p
+
+    signal.signal(signal.SIGINT, exit_gracefully)
+    signal.signal(signal.SIGTERM, exit_gracefully)
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED_PIN, GPIO.OUT)
+
+    p = GPIO.PWM(LED_PIN, 50)  # channel=12 frequency=50Hz
+    p.start(100)
 
 def exit_gracefully(signum, frame):
     cleanup()
@@ -14,15 +38,6 @@ def cleanup():
     p.stop()
     GPIO.cleanup()
     print("Cleaned up GPIO")
-
-signal.signal(signal.SIGINT, exit_gracefully)
-signal.signal(signal.SIGTERM, exit_gracefully)
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
-
-p = GPIO.PWM(LED_PIN, 50)  # channel=12 frequency=50Hz
-p.start(100)
 
 def breathe(count):
     for repeat in range(count):
@@ -63,7 +78,18 @@ def do_blink():
     blink(5)
 
 
+setup_sun()
+setup_gpio()
+
 while True:
+    sun = city.sun()
+
+    print('Dawn:    %s' % str(sun['dawn']))
+    print('Sunrise: %s' % str(sun['sunrise']))
+    print('Noon:    %s' % str(sun['noon']))
+    print('Dusk:    %s' % str(sun['dusk']))
+    print('Sunset:  %s' % str(sun['sunset']))
+
     do_breath()
     time.sleep(5)
     do_blink()
